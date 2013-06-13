d1c26c2be825bc9e873a8df474c552a7af28a895
HadiAsiaie <h.asiayfard@gmail.com>
2013-06-13 12:35:53 +0430

    Working extension

---
+++ del_node.js
@@ -0,0 +1,9 @@
+//these are ID of the tags that are needed to delete to make UI nice
+var ar=["gbzw","gbu","gbx1","gbx3","gbqfbwa","footer"];
+
+//Just iterate on Dom tree and delete the tag
+for(var i=0;i<ar.length;i++)
+{
+    element = document.getElementById(ar[i]);
+    element.parentNode.removeChild(element);
+}
